4229adfbddafffdd139121339ae5e53c846db191
Jing ZENG <144108743+JingZENG123@users.noreply.github.com>
2024-03-21 17:33:33 +0100

    update the settings

---
+++ app/src/main/java/com/example/learningapp/LearningAppScreen.kt
@@ -0,0 +1,113 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.learningapp
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.learningapp.ui.StudyViewModel
+import androidx.navigation.compose.NavHost
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.res.dimensionResource
+import androidx.navigation.compose.composable
+import com.example.learningapp.ui.RoomSettingScreen
+import androidx.annotation.StringRes
+import androidx.navigation.compose.currentBackStackEntryAsState
+import com.example.LearningApp.R
+
+enum class LearningScreen(@StringRes val title: Int){
+    Setting(title = R.string.app_name),
+    SingleRoom(title=R.string.single_room)
+}
+/**
+ * Composable that displays the topBar and displays back button if back navigation is possible.
+ */
+@Composable
+fun LearningAppBar(
+    currentScreen: LearningScreen,
+    canNavigateBack: Boolean,
+    navigateUp: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    TopAppBar(
+        title = { Text(stringResource(currentScreen.title)) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(
+            containerColor = MaterialTheme.colorScheme.primaryContainer
+        ),
+        modifier = modifier,
+        navigationIcon = {
+            if (canNavigateBack) {
+                IconButton(onClick = navigateUp) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = stringResource(R.string.back_button)
+                    )
+                }
+            }
+        }
+    )
+}
+
+@Composable
+fun LearningApp(
+    viewModel: StudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = LearningScreen.valueOf(
+        backStackEntry?.destination?.route ?: LearningScreen.Setting.name
+    )
+    Scaffold(
+        topBar = {
+            LearningAppBar(
+                currentScreen = currentScreen,
+                canNavigateBack = navController.previousBackStackEntry != null,
+                navigateUp = { navController.navigateUp() }
+            )
+        }
+    ) { innerPadding ->
+        val uiState by viewModel.uiState.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = LearningScreen.Setting.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            composable(route = LearningScreen.Setting.name) {
+                RoomSettingScreen(
+                    studyViewModel=viewModel,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(dimensionResource(R.dimen.padding_medium))
+                )
+            }
+        }
+    }
+}
